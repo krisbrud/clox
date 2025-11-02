ca283ecbb424d033730ac9e656a0b7375fe6d0e3
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-02 18:32:14 +0100

    Start work on scanner

--- compiler.c
+++ compiler.c
@@ -4,6 +4,22 @@
 
 #include "compiler.h"
 
+#include <stdio.h>
+
 void compile(const char *source) {
     initScanner(source);
+    int line = -1;
+
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ")
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start)
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
