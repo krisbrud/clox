6fd7d34ae393f375a8e8f4e687cae0cbd4eb1f5e
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-02 18:23:34 +0100

    Add compiler and change vm

---
+++ compiler.c
@@ -0,0 +1,9 @@
+//
+// Created by Kristian Brudeli on 02/11/2025.
+//
+
+#include "compiler.h"
+
+void compile(const char *source) {
+    initScanner(source);
+}
---
+++ compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by Kristian Brudeli on 02/11/2025.
+//
+
+#ifndef CLOX_COMPILER_H
+#define CLOX_COMPILER_H
+
+void compile(const char* source);
+
+#endif //CLOX_COMPILER_H
