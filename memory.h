5cf8a9ab16d28124eb5e7f7ba2cab4eeed82b6fe
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-02 12:37:21 +0100

    Implement FREE_ARRAY

--- memory.h
+++ memory.h
@@ -14,6 +14,9 @@
     (type*)reallocate(pointer, sizeof(type) * (oldCount), \
         sizeof(type) * (newCount))
 
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
 #endif //CLOX_MEMORY_H
