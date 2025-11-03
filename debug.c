a57f0f1b4534baefa75f00d78547942cc960f2cd
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-03 20:37:28 +0100

    Fix bug in dissassembleInstruction

--- debug.c
+++ debug.c
@@ -48,7 +48,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
         case OP_MULTIPLY:
-            return simpleInstruction("OP_NEGATE", offset);
+            return simpleInstruction("OP_MULTIPLY", offset);
         case OP_DIVIDE:
             return simpleInstruction("OP_DIVIDE", offset);
         case OP_RETURN:
