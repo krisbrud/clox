d77ad40b1cc65bce7f53e4e12cdd9b029e536a3f
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-09 15:24:21 +0100

    WIP chapter 18.4

--- compiler.c
+++ compiler.c
@@ -132,7 +132,9 @@ static void endCompiler() {
 
 // Forwards declarations
 static void expression();
-static ParseRule* getRule(TokenType type);
+
+static ParseRule *getRule(TokenType type);
+
 static void parsePrecedence(Precedence precedence);
 
 
@@ -151,6 +153,18 @@ static void binary() {
         case TOKEN_SLASH: emitByte(OP_DIVIDE);
             break;
         default: return;
+    }
+}
+
+static void literal() {
+    switch (parser.previous.type) {
+        case TOKEN_FALSE: emitByte(OP_FALSE);
+            break;
+        case TOKEN_NIL: emitByte(OP_NIL);
+            break;
+        case TOKEN_TRUE: emitByte(OP_TRUE);
+            break;
+        default: return; // unreachable
     }
 }
 
@@ -176,6 +190,8 @@ static void unary() {
     parsePrecedence(PREC_UNARY);
 
     switch (operatorType) {
+        case TOKEN_BANG: emitByte(OP_NOT);
+            break;
         case TOKEN_MINUS: emitByte(OP_NEGATE);
             break;
         default: return; // should be unreachable
@@ -211,7 +227,7 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
     [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
     [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
-    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG] = {unary, NULL, PREC_NONE},
     [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
     [TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
     [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
@@ -223,16 +239,17 @@ ParseRule rules[] = {
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
     [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
-    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {literal, NULL, PREC_NONE},
     [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
     [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
     [TOKEN_IF] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {literal, NULL, PREC_NONE},
     [TOKEN_OR] = {NULL, NULL, PREC_NONE},
     [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
     [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
-    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
     [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
     [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
@@ -240,7 +257,7 @@ ParseRule rules[] = {
 };
 
 
-static ParseRule* getRule(TokenType type) {
+static ParseRule *getRule(TokenType type) {
     return &rules[type];
 }
 
