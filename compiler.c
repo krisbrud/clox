c0de7badb9d875a03bfa85b74d8ebead3024cde3
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-16 16:32:04 +0100

    Add support for variable declarations

--- compiler.c
+++ compiler.c
@@ -142,13 +142,27 @@ static void endCompiler() {
 
 // Forward declarations
 static void expression();
+
 static void statement();
+
 static void declaration();
 
 static ParseRule *getRule(TokenType type);
 
 static void parsePrecedence(Precedence precedence);
 
+static uint8_t identifierConstant(Token* name) {
+    return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage) {
+    consume(TOKEN_IDENTIFIER, errorMessage);
+    return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global) {
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
 
 static void binary() {
     TokenType operatorType = parser.previous.type;
@@ -207,7 +221,7 @@ static void expression() {
 
 static void expressionStatement() {
     expression();
-    consume(TOKEN_SEMICOLON, "Expect ';' after expression.")
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
     emitByte(OP_POP);
 }
 
@@ -240,8 +254,25 @@ static void synchronize() {
     }
 }
 
+static void varDeclaration() {
+    uint8_t global = parseVariable("Expect variable name.");
+
+    if (match(TOKEN_EQUAL)) {
+        expression();
+    } else {
+        emitByte(OP_NIL);
+    }
+    consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration");
+
+    defineVariable(global);
+}
+
 static void declaration() {
-    statement();
+    if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        statement();
+    }
 
     if (parser.panicMode) synchronize();
 }
@@ -291,6 +322,7 @@ static void parsePrecedence(Precedence precedence) {
         infixRule();
     }
 }
+
 
 ParseRule rules[] = {
     [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
