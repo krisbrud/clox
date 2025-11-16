601a63825c30ed8270943b563f30c1c0d9c36cc6
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-16 15:03:10 +0100

    Add tableAddAll

--- table.c
+++ table.c
@@ -8,7 +8,7 @@
 
 #define TABLE_MAX_LOAD 0.75
 
-void initTable(Table* table) {
+void initTable(Table *table) {
     table->count = 0;
     table->capacity = 0;
     table->entries = NULL;
@@ -19,16 +19,50 @@ void freeTable(Table *table) {
     initTable(table);
 }
 
-static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
     uint32_t index = key->hash % capacity;
     for (;;) {
-        Entry* entry = &entries[index];
-        if (entry->key == key ||entry->key == NULL) {
+        Entry *entry = &entries[index];
+        if (entry->key == key || entry->key == NULL) {
             // Found the entry we are looking for or an empty spot
             return entry;
         }
 
         index = (index + 1) % capacity; // Increment index with wrap-around
+    }
+}
+
+static void adjustCapacity(Table *table, int capacity) {
+    // Initialize a new table
+    Entry *entries = ALLOCATE(Entry, capacity);
+    for (int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    // Copy over entries from the old table
+    for (int i = 0; i < table->capacity; i++) {
+        Entry *entry = &table->entries[i];
+        if (entry->key == NULL) continue;
+
+        Entry *dest = findEntry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+    }
+
+    // Free the old array
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
+void tableAddAll(Table *from, Table *to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry* entry = &from->entries[i];
+        if (entry->key != NULL) {
+            tableSet(to, entry->key, entry->value);
+        }
     }
 }
 
@@ -38,7 +72,7 @@ bool tableSet(Table *table, ObjString *key, Value value) {
         adjustCapacity(table, capacity);
     }
 
-    Entry* entry = findEntry(table->entries, table->capacity, key);
+    Entry *entry = findEntry(table->entries, table->capacity, key);
     bool isNewKey = entry->key == NULL;
     if (isNewKey) table->count++;
 
