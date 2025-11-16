3a31a3ed796df40944f9edb8d33a6c1c3c7b7fdd
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-16 15:36:00 +0100

    Implement string interning

--- table.c
+++ table.c
@@ -3,6 +3,9 @@
 //
 
 #include "table.h"
+
+#include <string.h>
+
 #include "memory.h"
 #include "value.h"
 
@@ -85,6 +88,26 @@ void tableAddAll(Table *from, Table *to) {
         if (entry->key != NULL) {
             tableSet(to, entry->key, entry->value);
         }
+    }
+}
+
+ObjString * tableFindString(Table *table, const char *chars, int length, uint32_t hash) {
+    if (table->count == 0) return NULL;
+
+    uint32_t index = hash % table->capacity;
+    for (;;) {
+        Entry* entry = &table->entries[index];
+        if (entry->key == NULL) {
+            // Stop if we find an empty non-tombstone entry.
+            if (IS_NIL(entry->value)) return NULL;
+        } else if (entry->key->length == length &&
+            entry->key->hash == hash &&
+            memcmp(entry->key->chars, chars, length) == 0) {
+            // found the string
+            return entry->key;
+        }
+
+        index = (index + 1) % table->capacity;
     }
 }
 
