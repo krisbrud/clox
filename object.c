da55eb924206ba54f9d6d31e163bc8522064a047
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-23 19:43:53 +0100

    Wrap functions in closure objects

--- object.c
+++ object.c
@@ -18,10 +18,15 @@ static Obj* allocateObject(size_t size, ObjType type) {
     Obj* object = (Obj*)reallocate(NULL, 0, size);
     object->type = type;
 
-
     object->next = vm.objects;
     vm.objects = object;
     return object;
+}
+
+ObjClosure * newClosure(ObjFunction* function) {
+    ObjClosure* closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+    closure->function = function;
+    return closure;
 }
 
 static ObjString* allocateString(char* chars, int length, uint32_t hash) {
@@ -90,6 +95,9 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_CLOSURE:
+            printFunction(AS_CLOSURE(value)->function);
+            break;
         case OBJ_FUNCTION:
             printFunction(AS_FUNCTION(value));
             break;
