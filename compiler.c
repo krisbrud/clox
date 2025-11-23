958dc7d89e6d7f75c5c8f096f2d8937613963794
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-23 20:12:07 +0100

    WIP on ch 25.2

--- compiler.c
+++ compiler.c
@@ -270,6 +270,17 @@ static int resolveLocal(Compiler *compiler, Token *name) {
     return -1; // Global variable
 }
 
+static int resolveUpvalue(Compiler* compiler, Token* name) {
+    if (compiler->enclosing == NULL) return -1; // Global scope, no upvalues
+
+    int local = resolveLocal(compiler->enclosing, name);
+    if (local != -1) {
+        return addUpvalue(compiler, (uint8_t)local, true);
+    }
+
+    return -1;
+}
+
 static void addLocal(Token name) {
     if (current->localCount == UINT8_COUNT) {
         error("Too many local variables in function");
@@ -433,6 +444,9 @@ static void namedVariable(Token name, bool canAssign) {
     if (arg != -1) {
         getOp = OP_GET_LOCAL;
         setOp = OP_SET_LOCAL;
+    } else if ((arg = resolveUpvalue(current, &name)) != -1) {
+        getOp = OP_GET_UPVALUE;
+        setOp = OP_SET_UPVALUE;
     } else {
         // global variable
         arg = identifierConstant(&name);
