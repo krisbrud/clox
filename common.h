783b65f6752423408e471b1b11bfbe418d53a8a4
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-25 21:49:19 +0100

    Add debug logs for gc

--- common.h
+++ common.h
@@ -12,6 +12,10 @@
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
+// Activating makes the GC run at every possible moment, useful for finding bugs
+#define DEBUG_STRESS_GC
+#define DEBUG_LOG_GC
+
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif //CLOX_COMMON_H
