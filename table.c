9660f20f39c1bd9e3deb4015058640083c5dbe12
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-30 20:26:57 +0100

    Implement hash table trick

--- table.c
+++ table.c
@@ -25,7 +25,7 @@ void freeTable(Table *table) {
 }
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1); // Hack since the size of the hashtable is a power of 2
     Entry *tombstone = NULL;
 
     for (;;) {
@@ -43,7 +43,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
             return entry;
         }
 
-        index = (index + 1) % capacity; // Increment index with wrap-around
+        index = (index + 1) & (capacity - 1); // Increment index with wrap-around
     }
 }
 
@@ -96,7 +96,7 @@ void tableAddAll(Table *from, Table *to) {
 ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t hash) {
     if (table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;) {
         Entry *entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -109,7 +109,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
