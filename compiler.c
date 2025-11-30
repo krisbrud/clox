940121027339a19895e04bb3e9f6a821b87c8e13
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-30 12:04:14 +0100

    Implement instances

--- compiler.c
+++ compiler.c
@@ -554,6 +554,18 @@ static void function(FunctionType type) {
     }
 }
 
+static void classDeclaration() {
+    consume(TOKEN_IDENTIFIER, "Expect class name.");
+    uint8_t nameConstant = identifierConstant(&parser.previous);
+    declareVariable();
+
+    emitBytes(OP_CLASS, nameConstant);
+    defineVariable(nameConstant);
+
+    consume(TOKEN_LEFT_BRACE, "Expect '{' before class body");
+    consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body");
+}
+
 static void funDeclaration() {
     printf("fundeclaration\n");
     uint8_t global = parseVariable("Expect function name.");
@@ -705,7 +717,9 @@ static void varDeclaration() {
 }
 
 static void declaration() {
-    if (match(TOKEN_FUN)) {
+    if (match(TOKEN_CLASS)) {
+        classDeclaration();
+    } else if (match(TOKEN_FUN)) {
         funDeclaration();
     } else if (match(TOKEN_VAR)) {
         varDeclaration();
--- memory.c
+++ memory.c
@@ -73,6 +73,11 @@ static void blackenObject(Obj *object) {
 #endif
 
     switch (object->type) {
+        case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            markObject((Obj*)klass->name);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *) object;
             markObject((Obj *) closure->function);
@@ -85,6 +90,12 @@ static void blackenObject(Obj *object) {
             ObjFunction *function = (ObjFunction *) object;
             markObject((Obj *) function->name);
             markArray(&function->chunk.constants);
+            break;
+        }
+        case OBJ_INSTANCE: {
+            ObjInstance* instance = (ObjInstance*)object;
+            markObject((Obj*)instance->klass);
+            markTable(&instance->fields);
             break;
         }
         case OBJ_UPVALUE:
@@ -117,6 +128,12 @@ static void freeObject(Obj *object) {
             ObjFunction *function = (ObjFunction *) object;
             freeChunk(&function->chunk);
             FREE(ObjFunction, object);
+            break;
+        }
+        case OBJ_INSTANCE: {
+            ObjInstance* instance = (ObjInstance*)object;
+            freeTable(&instance->fields);
+            FREE(ObjInstance, object);
             break;
         }
         case OBJ_NATIVE: {
