ae9429c524fdb1516d9757d6e761185b0aac96e1
Kristian Brudeli <krbrudeli@gmail.com>
2025-11-30 11:38:56 +0100

    Start implementing classes

--- memory.c
+++ memory.c
@@ -102,6 +102,11 @@ static void freeObject(Obj *object) {
 #endif
 
     switch (object->type) {
+        case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            markObject((Obj*)klass->name);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *) object;
             FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
